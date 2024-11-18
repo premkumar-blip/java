a16b3cd6e638f15b9de0e44b145c384ee77c6df7
premkumar-blip <beemanpremkumar@gmail.com>
2024-11-18 13:23:05 +0530

    Add files via upload

---
+++ AddTwonumber.java
@@ -0,0 +1,13 @@
+package javaprogramming;
+
+public class AddTwonumber {
+	public static void main(String[]args) {
+		addnumber(10,20);
+	}
+	 static void addnumber(int num1,int num2)
+	{
+		int sum=num1+num2;
+		System.out.println("sum of the value" + sum);		
+	}
+
+}
