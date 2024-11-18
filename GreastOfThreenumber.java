12d6f3a50125428e8da4ed522a4087fc9765a6fa
premkumar-blip <beemanpremkumar@gmail.com>
2024-11-18 13:24:08 +0530

    Add files via upload

---
+++ GreastOfThreenumber.java
@@ -0,0 +1,17 @@
+package javaprogramming;
+//task
+public class GreastOfThreenumber {
+	public static void main (String[]args) {
+	    int a=10,b=35,c=40;
+		if((a>b)&&(a>c)){
+			System.out.println("a is greater");		
+		}
+		else if((b>a)&&(b>c)) {
+			System.out.println("b is greater");
+		}
+		else {
+			System.out.println("c is greater");
+		}
+	}
+
+}
