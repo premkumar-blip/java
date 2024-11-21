26bf8dfee8b186daf373d92fc1f3f760bb979bc9
premkumar-blip <beemanpremkumar@gmail.com>
2024-11-21 13:51:53 +0530

    Add files via upload

---
+++ WhileloopTask.java
@@ -0,0 +1,13 @@
+package javaprogram;
+//write program to print 10,20,30,40,50,60,70,80,90,100//task
+import java.util.*;
+public class WhileloopTask {
+	public static void main(String[]args) {
+		int i=10;
+		while(i<=100) {
+			System.out.println(i);
+			i +=10;
+		}
+	}
+
+}
