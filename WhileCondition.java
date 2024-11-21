a6ffb143cd5f365655050f6cd57bc8b635460fea
premkumar-blip <beemanpremkumar@gmail.com>
2024-11-21 13:48:59 +0530

    Add files via upload

---
+++ WhileCondition.java
@@ -0,0 +1,13 @@
+package javaprogramming;
+
+public class WhileCondition {
+	public static void main(String[]args) {
+	int i=0;
+		while(i<10) {
+			System.out.print(i);
+			i++;
+			System.out.println("After increment"+i);
+			}
+	}
+
+}
