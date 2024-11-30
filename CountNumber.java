836fcd47d411536c826893e01b2c39f3c19af3b7
premkumar-blip <beemanpremkumar@gmail.com>
2024-11-30 07:36:48 +0530

    Add files via upload

---
+++ CountNumber.java
@@ -0,0 +1,10 @@
+package javaprogram;
+import java.util.*;
+public class CountNumber {
+	public static void main(String[] args) {
+		String str="premkumar";
+		int count=str.length();
+		System.out.println("the number of letter is :"+count);
+	}
+
+}
