8c2dacf6e503e01b0b02180f380e39d70d7300b3
premkumar-blip <beemanpremkumar@gmail.com>
2024-12-20 13:26:46 +0530

    Add files via upload

---
+++ TASK/Task/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package Program;
+
+//import Program.String;
+
+public class AddTwoNumbers {
+	public static void main(String[]args) {
+		addnumber(10,20);
+	}
+	 static void addnumber(int num1,int num2)
+	{
+		int sum=num1+num2;
+		System.out.println("sum of the value" + sum);		
+	}
+
+}
